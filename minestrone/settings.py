6cc3973aa92a3e126d9676125ce9b5a8647b7518
Louis Opter <louis@dotcloud.com>
2011-09-04 19:47:47 -0700

    Activate celery events, launch celerycam and use a PostgreSQL database to make the Celery Djang Admin Monitor working

--- minestrone/settings.py
+++ minestrone/settings.py
@@ -19,12 +19,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/dotcloud/data/dev.sqlite3',  # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'template1',
+        'USER': dotcloud_env['DOTCLOUD_DB_SQL_LOGIN'],
+        'PASSWORD': dotcloud_env['DOTCLOUD_DB_SQL_PASSWORD'],
+        'HOST': dotcloud_env['DOTCLOUD_DB_SQL_HOST'],
+        'PORT': int(dotcloud_env['DOTCLOUD_DB_SQL_PORT']),
     }
 }
 
